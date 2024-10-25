1f4344406e124a035ba2b758f0067226c0c9bbe6
MURUJ ALSHAIKH <Muruj4@gmail.com>
2024-10-26 01:06:51 +0300

    login and registeration

--- app/src/main/java/com/example/gamezone/Login.java
+++ app/src/main/java/com/example/gamezone/Login.java
@@ -1,65 +1,147 @@
 package com.example.gamezone;
 
-
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Login extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 9001;
     private FirebaseAuth mAuth;
-    private EditText et_email, et_password;
+    private GoogleSignInClient mGoogleSignInClient;
+    private EditText etEmail, etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
-        et_email = findViewById(R.id.email_Login);
-        et_password = findViewById(R.id.password_Login);
 
+        mAuth = FirebaseAuth.getInstance();
+        etEmail = findViewById(R.id.email_Login);
+        etPassword = findViewById(R.id.password_Login);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
-    public void registerPage(View view){
-//        Log.d("Test","Test");
-        Intent intent = new Intent(this, Registration.class);
-        startActivity(intent);
+
+    public void googleSignIn(View view) {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
-    public void loginMethod(View view) {
-        String email = et_email.getText().toString().trim();
-        String password = et_password.getText().toString().trim();
-        if(email.isEmpty()){
-            et_email.setError("Email is required");
-            et_email.requestFocus();
-            return;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
         }
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            et_email.setError("Please provide valid Email");
-            et_email.requestFocus();
-            return;
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            firebaseAuthWithGoogle(account);
+        } catch (ApiException e) {
+            Log.w("Login", "Google sign in failed", e);
+            Toast.makeText(this, "Google sign in failed.", Toast.LENGTH_SHORT).show();
         }
-        if(password.isEmpty() || password.length() < 6){ //you can also check the num. of chars.
-            et_password.setError("Password is required and must be > 6");
-            et_password.requestFocus();
-            return;
-        }
-        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
-                    //redirect to the mainpage
-                    Toast.makeText(getApplicationContext(), "CORRECT!", Toast.LENGTH_LONG).show();
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), task.getException().toString(), Toast.LENGTH_LONG).show();
+                if (task.isSuccessful()) {
+                    Toast.makeText(Login.this, "Google Sign-In successful", Toast.LENGTH_SHORT).show();
+                    saveUserToDatabase(acct);
+                    //intent
+                } else {
+                    Toast.makeText(Login.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    private void saveUserToDatabase(GoogleSignInAccount account) {
+        String uid = mAuth.getCurrentUser().getUid();
+        String name = account.getDisplayName();
+        String email = account.getEmail();
+        Player user = new Player(name, email);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users").child(uid);
+        databaseReference.setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(Login.this, "User has been registered!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Login.this, "Failed to save user data.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void loginMethod(View view) {
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            etEmail.setError("Email is required");
+            etEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.setError("Please provide a valid Email");
+            etEmail.requestFocus();
+            return;
+        }
+        if (password.isEmpty() || password.length() < 6) {
+            etPassword.setError("Password is required and must be > 6");
+            etPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Login Successful!", Toast.LENGTH_LONG).show();
+                    //intent
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login Failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void registerPage(View view) {
+        Intent intent = new Intent(this, Registration.class);
+        startActivity(intent);
     }
 }
